8f355bfb3ee3e03ca779fb7835b5bd6a1b10a967
deridiot <deridiot@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2007-05-23 18:34:52 +0000

    lil script to fix old anidb pic paths with the new ones
    git-svn-id: svn://dev.anidb.net/svn/trunk@119 bed53a55-4da3-433c-baf4-d4f70f2fd34a

---
+++ css/fix_old_anidb_pic_links.py
@@ -0,0 +1,20 @@
+'''
+lil script to fix old anidb pic paths with the new ones
+'''
+
+import os
+
+for root, dirs, filenames in os.walk(os.getcwd()):
+    for cssfile in filenames:
+        if cssfile.endswith('css'):
+            newfile = ''
+            css = file(root+'/'+cssfile,'rbU').readlines()
+            for line in css:
+                line = line.replace('http://www.anidb.net/css/anidbstyle/images/bg','../anidbstyle/images')
+                line = line.replace('http://anidb.net/css/anidbstyle/images/bg','../anidbstyle/images')
+                line = line.replace('http://www.anidb.net/css/anidbstyle/images','../icons')
+                line = line.replace('http://anidb.net/css/anidbstyle/images','../icons')
+                line = line.replace('http://www.anidb.net/pics','../icons/flags')
+                line = line.replace('http://anidb.net/pics','../icons/flags')
+                newfile +=line
+            file(root+'/'+cssfile,'wbU').write(newfile)
