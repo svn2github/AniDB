8e9fc2755a40b9052e8be6edcecc3a675056ff04
deridiot <deridiot@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2007-05-26 17:39:25 +0000

    -error handling for not existing pics (would write empty pics before) -follows @import lines
    git-svn-id: svn://dev.anidb.net/svn/trunk@130 bed53a55-4da3-433c-baf4-d4f70f2fd34a

--- css/scrapecss.py
+++ css/scrapecss.py
@@ -2,55 +2,82 @@
 lil css pic scraper
 -------------------
 feed it a file (scrapecss.txt) with links to css and it will look for "url("
-and download the files behind those. works also with relative pathes.
+and download the files behind those.
 download path is set in __base. so set accordingly
+
++ follows @import lines
++ works also with relative paths in the css files (tries to build the path)
+- It's not meant to work with local files. It could be changed to handle those, but
+that's hardly needed. if at lal only for tetsing purposes
 '''
 
 import os
-from urllib import urlopen
+from urllib2 import urlopen
 
 __base = 'D:/test'
+__pics = []
 
 def create_folder(path,position='/'):
     if path.find('/') >=0:
-	root,path = path.lstrip('/').split('/',1)
+        root,path = path.lstrip('/').split('/',1)
         if not os.path.exists(__base+position+'/'+root):
             os.chdir(__base+position)
             os.mkdir(root)
         create_folder(path,position+'/'+root)
 
-def get_url(link):
+def get_css(link):
     print 'now scraping', link
     if link.startswith('http://'):
         css = urlopen(link)
+        get_url(css,link)
     else:
-        css = file(link)
+        print 'ERROR - Expected url, got something else:', link
 
+def get_url(css,link):
     for line in css.readlines():
+        if line.find('@import')>=0:
+            i = line.find('"') + 1
+            j = line.rfind('"')
+            stuff = line[i:j].replace("'","").replace('"','')
+            if not stuff.find(':') >0:
+                root,crap = link.rsplit('/',1)
+                newlink = root + '/' + stuff
+            get_css(newlink)
         if line.find('url(')>0:
             i = line.find('(') + 1
             j = line.find(')')
             stuff = line[i:j].replace("'","").replace('"','')
-            if not stuff.startswith('http://www.anidb.net'):
-                if not stuff.startswith('http://'):
-                    root,crap = link.rsplit('/',1)
-                    stuff = root+'/'+stuff
-                download(stuff)
+            if not stuff.startswith('http://'):
+                root,crap = link.rsplit('/',1)
+                stuff = root + '/' + stuff
+            __pics.append(stuff)
+
+def remove_dupes():
+    __pics.sort()
+    for elem in __pics:
+        if __pics.count(elem) >1:
+            __pics.remove(elem)
+    for url in __pics:
+        download(url)
 
 def download(url):
-    print 'Downloading', url
-    pic = urlopen(url)
     try:
-        file(__base +'/'+ url.lstrip('http://'),'wb').write(pic.read())
+        pic = urlopen(url,'r')
+        try:
+            file(__base +'/'+ url.lstrip('http://'),'wb').write(pic.read())
+            print 'Downloading', url
+        except:
+            create_folder(url.lstrip('http://'))
+            download(url)
     except:
-        create_folder(url.lstrip('http://'))
-        download(url)
+        print 'ERROR - url not valid or file not existing:', url
 
 def read_stuff_to_do():
     scrape = file('scrapecss.txt','r')
     for line in scrape:
         if line != '':
-            get_url(line.rstrip('\n'))
+            get_css(line.rstrip('\n'))
 
 if __name__ == "__main__":
     read_stuff_to_do()
+    remove_dupes()
