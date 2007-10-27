88f2e224c89cea906c821ab12d700dcb17b8ac6e
epox <epox@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2007-10-27 10:41:55 +0000

    git-svn-id: svn://dev.anidb.net/svn/trunk@418 bed53a55-4da3-433c-baf4-d4f70f2fd34a

--- upd_clients/webaom/src/db01.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE job DROP INDEX FK_job_4, DROP FOREIGN KEY FK_job_4;
-ALTER TABLE anime MODIFY COLUMN genre VARCHAR(512) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL DEFAULT '';
-UPDATE anime SET english=NULL WHERE romaji=english OR english='';
-UPDATE mylist SET state=1 WHERE lid=0;
--- upd_clients/webaom/src/db04.sql
+++
@@ -1,9 +0,0 @@
-UPDATE jtb SET status=16777224 WHERE status=0;
-UPDATE jtb SET status=50331649 WHERE status=16;
-UPDATE jtb SET status=17891330 WHERE status=48;
-UPDATE jtb SET status=17956866 WHERE status=80;
-UPDATE jtb SET status=19005442 WHERE status=112;
-UPDATE jtb SET status=18939906 WHERE status=144;
-UPDATE jtb SET status=16777217 WHERE status=240;
-ALTER TABLE jtb ADD COLUMN avxml TEXT DEFAULT NULL;
-UPDATE vtb SET ver=2;
