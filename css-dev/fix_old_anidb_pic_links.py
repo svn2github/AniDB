7846fb80ca435a109b1aeddb74eb09caf75e8b94
deridiot <deridiot@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2008-01-09 00:37:51 +0000

    made a copy
    git-svn-id: svn://dev.anidb.net/svn/trunk@491 bed53a55-4da3-433c-baf4-d4f70f2fd34a

---
+++ css-dev/fix_old_anidb_pic_links.py
@@ -0,0 +1,26 @@
+'''
+lil script to fix old anidb pic paths with the new ones
+don't trust this too much. changed files should get checked manually
+with a diff tool afterwards
+'''
+
+import os
+
+for root, dirs, filenames in os.walk(os.getcwd()):
+    for cssfile in filenames:
+        if cssfile.endswith('css'):
+            newfile = ''
+            css = file(root+'/'+cssfile,'rb').readlines()
+            for line in css:
+                line = line.replace('div.icoco','div.icons')
+                line = line.replace('http://www.anidb.net/css/anidbstyle/images/bg','../anidbstyle/images')
+                line = line.replace('http://static.anidb.net/css/anidbstyle/images/bg','../anidbstyle/images')
+                line = line.replace('http://anidb.net/css/anidbstyle/images/bg','../anidbstyle/images')
+                line = line.replace('http://www.anidb.net/css/anidbstyle/images','../icons')
+                line = line.replace('http://static.anidb.net/css/anidbstyle/images','../icons')
+                line = line.replace('http://anidb.net/css/anidbstyle/images','../icons')
+                line = line.replace('http://www.anidb.net/pics','../icons/flags')
+                line = line.replace('http://static.anidb.net/pics','../icons/flags')
+                line = line.replace('http://anidb.net/pics','../icons/flags')
+                newfile +=line
+            file(root+'/'+cssfile,'wb').write(newfile)
