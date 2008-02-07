4d1e4ec82543377486835284d68d6214c18a0329
deridiot <deridiot@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2008-02-07 21:17:18 +0000

    git-svn-id: svn://dev.anidb.net/svn/trunk@558 bed53a55-4da3-433c-baf4-d4f70f2fd34a

--- js-dev/animedb.js
+++
@@ -1,9 +0,0 @@
-//(C) 2001 by EXPlorer <exp@3dwars.de>, all rights reserved!
-// for http://www.3dwars.de
-// VERSION: 0.02   20.06.2001
-//misc functions
-
-function popup(url,w,h,scr,re,title)
-{
-    window.open(url,title,'toolbar=0,location=0,directories=0,status=0,menubar=0,scrollbars='+scr+',resizable='+re+',width='+w+',height='+h);
-}
