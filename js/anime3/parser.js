5df02ca5b9c11d1ccb318a4efff226dc7db5874e
fahrenheit <fahrenheit@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2008-04-19 01:23:19 +0000

    opps i did it again :P
    git-svn-id: svn://dev.anidb.net/svn/trunk@882 bed53a55-4da3-433c-baf4-d4f70f2fd34a

--- js/anime3/parser.js
+++ js/anime3/parser.js
@@ -13,7 +13,6 @@
 var episodeFileTableRenderList = new Array(); // stack of fileTables in queue for render (currently not used)
 var fInt = 100; // time to wait between ep node parsing 
 var eInt = 150; // time to wait between ep render
-var groupOrder = new Array();
 
 // CORE Classes //
 
