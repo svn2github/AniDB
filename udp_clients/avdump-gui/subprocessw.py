e1954482d7a73c603628ff91cd1f0451dad7894b
pelican <pelican@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2008-07-23 18:54:48 +0000

    subprocessw: don't do ctypes junk unless actually on windows
    git-svn-id: svn://dev.anidb.net/svn/trunk@1278 bed53a55-4da3-433c-baf4-d4f70f2fd34a

--- udp_clients/avdump-gui/subprocessw.py
+++ udp_clients/avdump-gui/subprocessw.py
@@ -1,39 +1,39 @@
 import subprocess
 from subprocess import call, PIPE, STDOUT, list2cmdline, mswindows
-import ctypes
-import ctypes.wintypes
-CreateProcessW = ctypes.windll.kernel32.CreateProcessW
-CloseHandle = ctypes.windll.kernel32.CloseHandle
-
-__all__ = ["Popen", "PIPE", "STDOUT", "call"]
-
-class STARTUPINFOW(ctypes.Structure):
-	_fields_ = (("cb", ctypes.wintypes.DWORD), # should init to 17*4 maybe?
-		("lpReserved", ctypes.wintypes.LPWSTR),
-		("lpDesktop", ctypes.wintypes.LPWSTR),
-		("lpTitle", ctypes.wintypes.LPWSTR),
-		("dwX", ctypes.wintypes.DWORD),
-		("dwY", ctypes.wintypes.DWORD),
-		("dwXSize", ctypes.wintypes.DWORD),
-		("dwYSize", ctypes.wintypes.DWORD),
-		("dwXCountChars", ctypes.wintypes.DWORD),
-		("dwYCountChars", ctypes.wintypes.DWORD),
-		("dwFillAttribute", ctypes.wintypes.DWORD),
-		("dwFlags", ctypes.wintypes.DWORD),
-		("wShowWindow", ctypes.wintypes.WORD),
-		("cbReserved2", ctypes.wintypes.WORD),
-		("lpReserved2", ctypes.wintypes.LPSTR), # actually LPBYTE
-		("hStdInput", ctypes.wintypes.HANDLE),
-		("hStdOutput", ctypes.wintypes.HANDLE),
-		("hStdError", ctypes.wintypes.HANDLE))
-
-class PROCESS_INFORMATION(ctypes.Structure):
-	_fields_ = (("hProcess", ctypes.wintypes.HANDLE),
-		("hThread", ctypes.wintypes.HANDLE),
-		("dwProcessId", ctypes.wintypes.DWORD),
-		("dwThreadId", ctypes.wintypes.DWORD))
 
 if mswindows:
+	import ctypes
+	import ctypes.wintypes
+	CreateProcessW = ctypes.windll.kernel32.CreateProcessW
+	CloseHandle = ctypes.windll.kernel32.CloseHandle
+
+	__all__ = ["Popen", "PIPE", "STDOUT", "call"]
+
+	class STARTUPINFOW(ctypes.Structure):
+		_fields_ = (("cb", ctypes.wintypes.DWORD), # should init to 17*4 maybe?
+			("lpReserved", ctypes.wintypes.LPWSTR),
+			("lpDesktop", ctypes.wintypes.LPWSTR),
+			("lpTitle", ctypes.wintypes.LPWSTR),
+			("dwX", ctypes.wintypes.DWORD),
+			("dwY", ctypes.wintypes.DWORD),
+			("dwXSize", ctypes.wintypes.DWORD),
+			("dwYSize", ctypes.wintypes.DWORD),
+			("dwXCountChars", ctypes.wintypes.DWORD),
+			("dwYCountChars", ctypes.wintypes.DWORD),
+			("dwFillAttribute", ctypes.wintypes.DWORD),
+			("dwFlags", ctypes.wintypes.DWORD),
+			("wShowWindow", ctypes.wintypes.WORD),
+			("cbReserved2", ctypes.wintypes.WORD),
+			("lpReserved2", ctypes.wintypes.LPSTR), # actually LPBYTE
+			("hStdInput", ctypes.wintypes.HANDLE),
+			("hStdOutput", ctypes.wintypes.HANDLE),
+			("hStdError", ctypes.wintypes.HANDLE))
+
+	class PROCESS_INFORMATION(ctypes.Structure):
+		_fields_ = (("hProcess", ctypes.wintypes.HANDLE),
+			("hThread", ctypes.wintypes.HANDLE),
+			("dwProcessId", ctypes.wintypes.DWORD),
+			("dwThreadId", ctypes.wintypes.DWORD))
 	class Popen(subprocess.Popen):
 		def _execute_child(self, args, executable, preexec_fn, close_fds,
 				cwd, env, universal_newlines,
