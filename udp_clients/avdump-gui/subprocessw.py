23afc00f19beefcd10578a5d09a341a070ccf247
pelican <pelican@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2008-07-30 15:58:14 +0000

    New version from upstream... meaning rar.
    git-svn-id: svn://dev.anidb.net/svn/trunk@1308 bed53a55-4da3-433c-baf4-d4f70f2fd34a

--- udp_clients/avdump-gui/subprocessw.py
+++ udp_clients/avdump-gui/subprocessw.py
@@ -1,14 +1,15 @@
 import subprocess
 from subprocess import call, PIPE, STDOUT, list2cmdline, mswindows
 
+__all__ = ["Popen", "PIPE", "STDOUT", "call"]
+
 if mswindows:
 	import ctypes
 	import ctypes.wintypes
 	CreateProcessW = ctypes.windll.kernel32.CreateProcessW
 	CloseHandle = ctypes.windll.kernel32.CloseHandle
-
-	__all__ = ["Popen", "PIPE", "STDOUT", "call"]
-
+	GetLastError = ctypes.windll.kernel32.GetLastError
+	
 	class STARTUPINFOW(ctypes.Structure):
 		_fields_ = (("cb", ctypes.wintypes.DWORD), # should init to 17*4 maybe?
 			("lpReserved", ctypes.wintypes.LPWSTR),
@@ -24,7 +25,7 @@ if mswindows:
 			("dwFlags", ctypes.wintypes.DWORD),
 			("wShowWindow", ctypes.wintypes.WORD),
 			("cbReserved2", ctypes.wintypes.WORD),
-			("lpReserved2", ctypes.wintypes.LPSTR), # actually LPBYTE
+			("lpReserved2", ctypes.POINTER(ctypes.wintypes.BYTE)), # LPBYTE
 			("hStdInput", ctypes.wintypes.HANDLE),
 			("hStdOutput", ctypes.wintypes.HANDLE),
 			("hStdError", ctypes.wintypes.HANDLE))
@@ -34,6 +35,7 @@ if mswindows:
 			("hThread", ctypes.wintypes.HANDLE),
 			("dwProcessId", ctypes.wintypes.DWORD),
 			("dwThreadId", ctypes.wintypes.DWORD))
+	
 	class Popen(subprocess.Popen):
 		def _execute_child(self, args, executable, preexec_fn, close_fds,
 				cwd, env, universal_newlines,
@@ -55,10 +57,10 @@ if mswindows:
 			if startupinfo == None:
 				startupinfo = STARTUPINFOW()
 			else:
-				raise NotImplementedError("Can't pass startup stuff")
+				raise NotImplementedError("Can't pass startup stuff for unicode args")
 			
 			if not None in (p2cread, c2pwrite, errwrite):
-				raise NotImplementedError("Can't pass file object bits")
+				raise NotImplementedError("Can't pass file object bits for unicode args")
 
 			if shell:
 				raise NotImplementedError("Can't pick your own shell for unicode args")
@@ -66,12 +68,13 @@ if mswindows:
 			margs = ctypes.create_unicode_buffer(args)
 			processinfo = PROCESS_INFORMATION()
 
-			CreateProcessW(executable, margs,
-				None, None,
-				1,
-				creationflags,
-				env, cwd,
+			ret = CreateProcessW(executable and unicode(executable), margs,
+				None, None, 1,
+				creationflags, env, cwd and unicode(cwd),
 				ctypes.byref(startupinfo), ctypes.byref(processinfo))
+			
+			if not ret:
+				raise ctypes.WinError()
 			
 			hp, ht, pid, tid = (processinfo.hProcess, processinfo.hThread,
 				processinfo.dwProcessId, processinfo.dwThreadId)
@@ -81,4 +84,5 @@ if mswindows:
 			self.pid = pid
 			CloseHandle(ht) # ht.Close()
 else:
+	# nix handling should probably just be .encode('utf8')
 	Popen = subprocess.Popen
