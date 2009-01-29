6b90eaca1673d26cd1b61b74fa2bc31200359c01
fahrenheit <fahrenheit@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2009-01-29 19:54:28 +0000

    [FEAT] some improvements for "add file relation" page ([tracker:1397:1397]) (anidb 0.1.55)
    git-svn-id: svn://dev.anidb.net/svn/trunk@2109 bed53a55-4da3-433c-baf4-d4f70f2fd34a

--- js/anime3/addfilerel.js
+++ js/anime3/addfilerel.js
@@ -17,11 +17,15 @@ var uriObj = new Array();      // Object that holds the URI
 var seeDebug = false;
 var relType, relFile;
 var fileType;
+var fileEp;
+var fileGid;
+var fileAid;
 var fileList;
 var relList;
 
-function filterFileList(type) {
+function filterFileList(type,filterEp) {
 	while (relFile.childNodes.length) relFile.removeChild(relFile.childNodes[0]);
+	var blah = 0;
 	for (var i = 0; i < fileList.options.length; i++) {
 		var option = fileList.options[i].cloneNode(true);
 		// Let me just state this once more, i hate IE...
@@ -29,22 +33,28 @@ function filterFileList(type) {
 		if (type == 'all' || option.value == -1) {
 			relFile.appendChild(option);
 		} else {
+			var fep = option.text.substring(option.text.indexOf('EP')+3,option.text.indexOf(' ',option.text.indexOf('EP')+3));
 			var ftype = option.text.substr(option.text.indexOf('TYPE')+5,option.text.length);
-			if (ftype.indexOf(type) >= 0) relFile.appendChild(option); 
+			var fgid = option.text.substring(option.text.indexOf('GID')+4,option.text.indexOf(' ',option.text.indexOf('GID')+4));
+			if (ftype.indexOf(type) >= 0 && 
+					(!fileGid ? true : (Number(fgid) == fileGid)) && // (Number(fgid) == fileGid || Number(fgid) == 0)
+					(!filterEp ? true : (fep == fileEp)))
+				relFile.appendChild(option); 
 		}
 	}
 }
 
 function filterFilesByRelationType() {
 	var type = 'all';
+	var filterEp = false;
 	switch (this.value) {
 		case  '10': 
-		case  '40':
+		case  '40': 
 		case  '50': type = 'video'; break;
-		case  '20': type = fileType; break;
+		case  '20': type = fileType; filterEp = true; break;
 		default   : type = 'all';
 	}
-	filterFileList(type);
+	filterFileList(type,filterEp);
 }
 
 function cleanFilterRelations() {
@@ -66,15 +76,29 @@ function cleanFilterRelations() {
 
 function prepPage() {
 	uriObj = parseURI(); // update the uriObj
-	if (uriObj['show'] != 'file' || uriObj['show.filereladd'] != 1) return; // badPage
-	relType = getElementsByName(document.getElementsByTagName('select'),'frel.reltype',true)[0];
-	relFile = getElementsByName(document.getElementsByTagName('select'),'frel.otherfid',true)[0];
-	fileType = getElementsByName(document.getElementsByTagName('input'),'ftype',true)[0];
+	if (uriObj['ajax'] && uriObj['ajax'] == 0) return;
+	var fileSection = getElementsByClassName(document.getElementsByTagName('div'),'g_section file_add_rel',false)[0];
+	if (!fileSection) return;
+	relType = getElementsByName(fileSection.getElementsByTagName('select'),'frel.reltype',true)[0];
+	relFile = getElementsByName(fileSection.getElementsByTagName('select'),'frel.otherfid',true)[0];
+	fileType = getElementsByName(fileSection.getElementsByTagName('input'),'ftype',true)[0];
+	fileGid = getElementsByName(fileSection.getElementsByTagName('input'),'gid',true)[0];
+	
+	var a = getElementsByClassName(fileSection.getElementsByTagName('a'),'anime',false)[0];
+	if (a) fileAid = Number(a.href.substring(a.href.indexOf('aid=')+4));
+	a = getElementsByClassName(fileSection.getElementsByTagName('a'),'episode',false)[0];
+	if (a) {
+		fileEp = a.firstChild.nodeValue.substring(0,a.firstChild.nodeValue.indexOf(' '));
+		while (fileEp.charAt(0) == '0')
+			fileEp = fileEp.substring(1);
+	}
+
 	fileList = relFile.cloneNode(true); // Backup :)
 	relList = relType.cloneNode(true); // Backup :)
-	if (!relType || !relFile || !fileType) return; // Some error
+	if (!relType || !relFile || !fileType || !fileGid) return; // Some error
 	relType.onchange = filterFilesByRelationType;
 	fileType = fileType.value;
+	fileGid = fileGid.value;
 	cleanFilterRelations();
 }
 
