77427b6adc2a1ad498c2b1c0cf6aaa01ff425fdc
fahrenheit <fahrenheit@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2009-02-18 16:28:44 +0000

    tag search
    git-svn-id: svn://dev.anidb.net/svn/trunk@2248 bed53a55-4da3-433c-baf4-d4f70f2fd34a

--- js-dev/anime3/tags.js
+++ js-dev/anime3/tags.js
@@ -1,14 +1,15 @@
 /* file common tags interfaces and functions
  * @author fahrenheit (alka.setzer@gmail.com)
  * version 1.0 (27.05.2008) - Initial Version
+ * version 1.2 (18.02.2009) - Update to reflect current tag system changes
  */
 jsVersionArray.push({
 	"file":"anime3/tags.js",
-	"version":"1.0",
+	"version":"1.2",
 	"revision":"$Revision$",
 	"date":"$Date::                           $",
 	"author":"$Author$",
-	"changelog":"Initial version"
+	"changelog":"Update to reflect current tag system changes"
 });
 
 var tags = new Array();
@@ -17,15 +18,17 @@ var searchString = "";
 var inactiveSearch = true;
 var override = false;
 var inputBox = null;
-var radioBox = null;
-var radioBox2 = null;
+var TagIsSpoiler = null;
+var TagIsASpoiler = null;
+var TagIsVerified = null;
+var TagDescription = null;
 var resultsDiv = null;
 
 /* Function that fetches data */
 function fetchData(searchString) {
 	var req = xhttpRequest();
 	if (''+window.location.hostname == '') xhttpRequestFetch(req, 'xml/tagsearch.xml', parseData);
-	else xhttpRequestFetch(req, 'animedb.pl?show=xml&t=tagsearch&search='+escape(searchString), parseData);
+	else xhttpRequestFetch(req, 'animedb.pl?show=xmln&t=search&type=tag&search='+escape(searchString), parseData);
 }
 
 /* XMLHTTP RESPONSE parser
@@ -33,7 +36,7 @@ function fetchData(searchString) {
  */
 function parseData(xmldoc) {
 	var root = xmldoc.getElementsByTagName('root').item(0);
-	if (!root) { if (seeDebug) alert('Error: Could not get root node'); return; }
+	if (!root) { errorAlert('parseData','Could not get root node'); return; }
 	var tagNodes = root.getElementsByTagName('tag');
 	tags = new Array();
 	for (var t = 0; t < tagNodes.length; t++)
@@ -68,9 +71,17 @@ function showResults() {
 		span.onclick = function onclick(event) {
 			var id = Number(this.id.substr(4,this.id.length));
 			var tag = tags[id];
+			/*
+			alert('tag.name: '+tag.name+
+				'\ntag.is_spoiler: '+tag.is_spoiler+
+				'\ntag.is_verified: '+tag.is_verified+
+				'\ntag.desc: '+tag.desc+
+				'\ntag.id: '+tag.id);
+			*/
 			inputBox.value = tag.name;
-			radioBox.checked = tag.is_spoiler;
-			radioBox2.checked = !tag.is_spoiler;
+			TagIsSpoiler.checked = tag.is_spoiler;
+			TagIsVerified.checked = tag.is_verified;
+			TagDescription.value = tag.desc;
 			resultsDiv.style.display = 'none';
 		}
 		span.ondblclick = function() { this.onclick(); };
@@ -87,7 +98,7 @@ function showResults() {
 }
 
 function checkSearchString() {
-	if (this.value.length < 4 && !override) {
+	if (this.value.length < 2 && !override) {
 		resultsDiv = document.getElementById('resultsDiv');
 		if (resultsDiv) resultsDiv.parentNode.removeChild(resultsDiv);
 		return;
@@ -110,21 +121,25 @@ function CTag(node) {
 	this.id = Number(node.getAttribute('id'));
 	this.is_spoiler = Number(node.getAttribute('is_spoiler'));
 	this.name = node.getAttribute('name');
+	this.desc = node.getAttribute('description') || "";
+	this.is_verified = Number(node.getAttribute('is_verified'));
 }
 
 /* Function that prepares the page */
 function prepPage() {
 	var divs = document.getElementsByTagName('div');
-	var divAddtag = getElementsByClassName(divs, 'animetag_add', true)[0];
+	var divAddtag = getElementsByClassName(divs, 'animetag_all', true)[0];
 	if (divAddtag) {
 		var inputs = divAddtag.getElementsByTagName('input');
-		inputBox = getElementsByName(inputs, 'addtag.name', true)[0];
-		radioBox = getElementsByName(inputs, 'addtag.spoiler', true)[0];
-		radioBox2 = getElementsByName(inputs, 'addtag.spoiler', true)[1];
+		inputBox = getElementsByName(inputs, 'tag.name', true)[0];
+		TagDescription = getElementsByName(divAddtag.getElementsByTagName('textarea'), 'tag.description', true)[0];
+		TagIsSpoiler = getElementsByName(inputs, 'tag.is_spoiler', true)[0];
+		TagIsASpoiler = getElementsByName(inputs, 'atag.is_spoiler', true)[0];
+		TagIsVerified = getElementsByName(inputs, 'tag.is_verified', true)[0];
 		inputBox.onkeyup = checkSearchString;
 	}
 	// more stuff for me to worry, sorting
-	var divTagList = getElementsByClassName(divs, 'animetag_list', true)[0];
+	var divTagList = getElementsByClassName(divs, 'animetag_entries', true)[0];
 	if (divTagList) {
 		var tables = divTagList.getElementsByTagName('table');
 		for (var t = 0; t < tables.length; t++) {
@@ -136,10 +151,11 @@ function prepPage() {
 			// now add the sorting things
 			var ths = thead.getElementsByTagName('th');
 			ths[0].className += ' c_latin'; // name
-			ths[1].className += ' c_latin'; // spoiler
+			ths[1].className += ' c_latin'; // desc
 			ths[2].className += ' c_number'; // approval
 			ths[3].className += ' c_number'; // global cnt
-			ths[4].className += ' c_date'; // date
+			ths[4].className += ' c_setlatin'; // verified
+			//ths[6].className += ' c_date'; // date
 			init_sorting(thead,'name','down')
 		}
 	}
