5e53ff2e3ffaf999bbc7e91b4064e9825e29ccf3
deridiot <deridiot@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2009-04-09 17:45:48 +0000

    git-svn-id: svn://dev.anidb.net/svn/trunk@2432 bed53a55-4da3-433c-baf4-d4f70f2fd34a

--- css-dev/cssmerge.py
+++ css-dev/cssmerge.py
@@ -1,6 +1,6 @@
 import os, sys
 
-__out = "../../flat/"
+__out = "E:/Program Files/Apache/htdocs/css/"
 
 def cssmerge(fullpath, outfile):
     path, filename = os.path.split(os.path.normpath(fullpath))
