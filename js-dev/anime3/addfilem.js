6a7dc65825316e665087b30c8e045fbd1c34d1d3
fahrenheit <fahrenheit@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2009-05-01 14:05:36 +0000

    okay, i think this is all i need from the client side
    git-svn-id: svn://dev.anidb.net/svn/trunk@2630 bed53a55-4da3-433c-baf4-d4f70f2fd34a

--- js-dev/anime3/addfilem.js
+++ js-dev/anime3/addfilem.js
@@ -39,6 +39,7 @@ var vidSection = null;
 var audSection = null;
 var subSection = null;
 var nextButton = null;
+var my_dump = null;
 
 /* Function that fetches data */
 function fetchData() {
@@ -191,10 +192,19 @@ function parseDump(text) {
 	table.appendChild(tbody);
 	div.appendChild(table);
 	// create Back/Add Files buttons
-	div.appendChild(createButton(null,'results_back',false,'Back','button',function onclick() { alert('i should be going back :/'); }, null));
+	div.appendChild(createButton(null,'results_back',false,'Back','button',restore_dump, null));
 	div.appendChild(document.createTextNode(' '));
 	div.appendChild(createButton('addf.mass','results_submit',false,'Add files','submit'));
+	div.appendChild(createButton('addfilem.action','results_hidden',false,'1','hidden'));
 	form.replaceChild(div,my_dump);
+}
+
+function restore_dump() {
+	if (!my_dump) return;
+	var my_result = document.getElementById('addfilem.result');
+	if (!my_result) return;
+	my_result.parentNode.replaceChild(my_dump,my_result);
+	nextButton.disabled = false;
 }
 
 /* [ INTERFACE ] */
