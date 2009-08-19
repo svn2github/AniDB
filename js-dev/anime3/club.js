da81f99163a54a420e27d3a02f79db7ff227e49e
noec <noec@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2009-08-19 19:15:29 +0000

    Added two files to js-dev tree. Related tracker entries: http://tracker.anidb.net/view.php?id=1411 http://tracker.anidb.net/view.php?id=1596
    git-svn-id: svn://dev.anidb.net/svn/trunk@2984 bed53a55-4da3-433c-baf4-d4f70f2fd34a

---
+++ js-dev/anime3/club.js
@@ -0,0 +1,23 @@
+jsVersionArray.push({
+	"file":"anime3/club.js",
+	"version":"0.1"
+});
+
+var userlistCols = {	"name user":{"type":'c_latin'},
+						//"level":{"type":'c_latin'}, //sorting as c_latin is not quite right
+						"name user added":{"type":'c_latin'},
+						"date":{"type":'c_date'}};
+
+var sortingCols = {};
+var tableNames = ["member", "exmember", "invited"];
+var skipTables = tableNames;
+
+function prepPage() {
+	for (var i = 0; i < tableNames.length; i++) {
+		sortingCols[tableNames[i]] = userlistCols;
+	}
+	handleTables(sortingCols, tableNames, skipTables, false, false);
+}
+
+// hook up the window onload event
+addLoadEvent(prepPage);
