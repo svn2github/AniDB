ad5695f9444c70793673eb8ffb7fd6a0bd6136b4
noec <noec@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2009-09-04 22:43:00 +0000

    JS sorting http://tracker.anidb.net/view.php?id=1410
    git-svn-id: svn://dev.anidb.net/svn/trunk@3047 bed53a55-4da3-433c-baf4-d4f70f2fd34a

---
+++ js-dev/anime3/mynotifies.js
@@ -0,0 +1,35 @@
+jsVersionArray.push({
+	"file":"anime3/notifies.js",
+	"version":"0.1",
+	"revision":"$Revision$",
+	"date":"$Date::                           $",
+	"author":"$Author$"
+});
+
+var sortingCols = {
+	'mynotifies_list': {	"name anime":{"type":'c_setlatin',"isDefault":true},
+                            "type":{"type":'c_latin'},
+                            "priority":{"type":'c_set'},
+                            "mylist sortable":{"type":'c_set'}, // anidbscript.js adds class 'sortable'
+                            "seen sortable":{"type":'c_set'},   // before handleTables() is called
+                            "state":{"type":'c_latin'},
+                            "date":{"type":'c_date'}}
+};
+var tableNames = ["mynotifies_list"];
+var skipTables = null;
+
+function prepPage() {
+    var table = getElementsByClassName(document.getElementsByTagName('table'),'mynotifies_list',false)[0];
+    if (!table) return;
+    table.id = 'mynotifies_list';
+	var tfoot = document.createElement('tfoot');
+	tfoot.appendChild(table.tBodies[0].rows[table.tBodies[0].rows.length-1]);
+	table.appendChild(tfoot);
+	
+	initTooltips();
+	createPreferencesTable('global');
+	handleTables(sortingCols, tableNames, skipTables, collapseThumbnails, (get_info & 2));
+}
+
+// hook up the window onload event
+addLoadEvent(prepPage);
