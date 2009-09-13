7fb5fca95cc95bc3fa9614561a9882a6166d6a6f
noec <noec@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2009-09-13 17:23:23 +0000

    JS sorting on all My Notifies pages.
    Sub-kfuaw CSS style update.
    
    git-svn-id: svn://dev.anidb.net/svn/trunk@3079 bed53a55-4da3-433c-baf4-d4f70f2fd34a

--- js-dev/anime3/mynotifies.js
+++ js-dev/anime3/mynotifies.js
@@ -1,5 +1,5 @@
 jsVersionArray.push({
-	"file":"anime3/notifies.js",
+	"file":"anime3/mynotifies.js",
 	"version":"0.1",
 	"revision":"$Revision$",
 	"date":"$Date::                           $",
@@ -7,24 +7,44 @@ jsVersionArray.push({
 });
 
 var sortingCols = {
-	'mynotifies_list': {	"name anime":{"type":'c_setlatin',"isDefault":true},
-                            "type":{"type":'c_latin'},
-                            "priority sortable":{"type":'c_set'},
-                            "mylist sortable":{"type":'c_set'}, // anidbscript.js adds class 'sortable'
-                            "seen sortable":{"type":'c_set'},   // before handleTables() is called
-                            "state":{"type":'c_latin'},
-                            "date":{"type":'c_date'}}
+	'anime_related': {	"name anime":{"type":'c_latin',"isDefault":true},
+                        "name group":{"type":'c_latin',"isDefault":true},
+                        "type":{"type":'c_latin'},
+                        "priority sortable":{"type":'c_set'}, // anidbscript.js adds class 'sortable'
+                        "mylist sortable":{"type":'c_set'},   // before handleTables() is called
+                        "seen sortable":{"type":'c_set'},
+                        "state":{"type":'c_latin'},
+                        "date":{"type":'c_date'}},
+	'forum_related': {	"name":{"type":'c_latin',"isDefault":true},
+                        "topics":{"type":'c_number'},
+                        "replies":{"type":'c_number'},
+                        "sendreplies":{"type":'c_latin'},
+                        "since":{"type":'c_date'},
+                        "lastpost":{"type":'c_date'}}
 };
-var tableNames = ["mynotifies_list"];
-var skipTables = null;
+var tableNames = ["anime_related", "forum_related"];
+var skipTables = ["forum_related"];
 
 function prepPage() {
-    var table = getElementsByClassName(document.getElementsByTagName('table'),'mynotifies_list',false)[0];
-    if (!table) return;
-    table.id = 'mynotifies_list';
-	var tfoot = document.createElement('tfoot');
-	tfoot.appendChild(table.tBodies[0].rows[table.tBodies[0].rows.length-1]);
-	table.appendChild(tfoot);
+    var tables = getElementsByClassName(document.getElementsByTagName('table'),'mynotifies_list',true);
+	if (!tables) return;
+    
+    for (var i = 0; i < tables.length; i++) {
+        var tableName = tables[i].className.split(' ')[1];
+        switch (tableName) {
+            case 'group':
+            case 'anime':
+                var tfoot = document.createElement('tfoot');
+                tfoot.appendChild(tables[i].tBodies[0].rows[tables[i].tBodies[0].rows.length-1]);
+                tables[i].appendChild(tfoot);
+                tables[i].id = 'anime_related'; break;
+            case 'forumblog':
+            case 'forumforum':
+            case 'forumclub':
+            case 'threads':
+                tables[i].id = 'forum_related'; break;
+        }
+    }
 	
 	initTooltips();
 	createPreferencesTable('global');
