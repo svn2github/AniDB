1a591fbe393eb0f8e671dde6c0d499ff88482d01
arokh <arokh@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2010-01-06 16:54:52 +0000

    Now with log:
    AVDump2Lib\Hashing\Algorithms\AICH.cs
    - Another futile attempt to implement AICH
    
    AVDump2Lib\Hashing\Tools\BaseConverter.cs
    - Rewritten BaseConverter (Convertes any bytearray to its specified base string equivalent)
    
    AVDump2Lib\Information\Info.cs
    - New Log Format (XML Formated MediaInfoLib output)
    
    AVDump2Lib\Information\Parser\MediaInfoLib\Enums.cs | MediaInfo.cs | MediaInfoList.cs
    - Added StreamType 'Menu' (Contains chapter info)
    
    AVDumpCL\CL.cs
    - New Log Formats: XML Formated MIL output, HashLog (Custimizable)
    - New class "NullStream" for testing purposes
    
    
    git-svn-id: svn://dev.anidb.net/svn/trunk@3209 bed53a55-4da3-433c-baf4-d4f70f2fd34a

--- udp_clients/AVDump2/AVDumpCL/CL.cs
+++ udp_clients/AVDump2/AVDumpCL/CL.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (C) 2009 DvdKhl
+﻿// Copyright (C) 2009 DvdKhl 
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
