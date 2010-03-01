00340492ffcff47684536a321b1bdb79d676025d
antennen <antennen@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2010-03-01 00:17:54 +0000

    Reverting
    git-svn-id: svn://dev.anidb.net/svn/trunk@3541 bed53a55-4da3-433c-baf4-d4f70f2fd34a

--- js/anime3/anime.js
+++ js/anime3/anime.js
@@ -1435,11 +1435,8 @@ function parseEpisodeData(xmldoc) {
 			if (!episode) continue;
 			var eprow = document.getElementById('eid_'+episode.id);
 			if (!eprow) { errorAlert('parseEpisodeData','no episode row for eid: '+episode.id); continue; } // no episode row for some reason
-			var a = getElementsByClassName(eprow.getElementsByTagName('a'), "expand", true);
-			if (a) {
-				a = a[0];
-				a.onclick = foldEp;
-			}
+			var a = eprow.getElementsByTagName('a')[0];
+			if (a) a.onclick = foldEp;
 			if (loadExpand) { // Normal behaviour
 				var eprowid = eprow.rowIndex + 1;
 				var tbRow = document.getElementById('eid_'+eid+'_ftHolder');
