b3a4457c39dee13a68022d725768a9375b3ab04e
deridiot <deridiot@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2010-03-20 16:37:00 +0000

    git-svn-id: svn://dev.anidb.net/svn/trunk@3680 bed53a55-4da3-433c-baf4-d4f70f2fd34a

---
+++ js/creq.js
@@ -0,0 +1,39 @@
+jsVersionArray.push({
+	"file":"creq.js",
+	"version":"1.0",
+	"revision":"$Revision: 2128 $",
+	"date":"$Date:: 2009-01-31 15:09:06 +0000#$",
+	"author":"$Author: fahrenheit $",
+	"changelog":"Initial version"
+});
+
+window.onload = (function()
+{
+	InitDefault();
+	my_mark = false;
+	var form = document.getElementById('type.del');
+	if( form )
+	{
+		add_check('do.del.notify.all');
+		form.onsubmit = do_submit;
+	}
+});
+function do_mark()
+{
+	my_mark = true;
+}
+function do_submit(event)
+{
+	if( my_mark)
+	{
+		my_mark = false;
+		return confirm("This will delete ALL notifies. Continue?");
+	}
+	return true;
+}
+function add_check(name)
+{
+	var elements = document.getElementsByName(name);
+	for (var i = 0, elem = null; (elem = elements[i]); i++)
+		elem.onclick = do_mark;	
+}
