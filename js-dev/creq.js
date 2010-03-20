b23d30f15735fa835e51e2afbc9fa0b190fdff2a
deridiot <deridiot@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2010-03-20 16:16:26 +0000

    git-svn-id: svn://dev.anidb.net/svn/trunk@3677 bed53a55-4da3-433c-baf4-d4f70f2fd34a

---
+++ js-dev/creq.js
@@ -0,0 +1,35 @@
+jsVersionArray.push({
+	"file":"creqmod.js",
+	"version":"1.0",
+	"revision":"$Revision: 2128 $",
+	"date":"$Date:: 2009-01-31 15:09:06 +0000#$",
+	"author":"$Author: fahrenheit $",
+	"changelog":"Initial version"
+});
+
+window.onload = (function(){
+	InitDefault();
+	my_mark = false;
+	var form = document.getElementById('type.del')
+	if( form ){
+		add_check('creq.add');
+		add_check('creq.add.close');
+		add_check('creq.add.next');
+		form.onsubmit = do_submit;
+	}
+});
+function do_mark(){
+	my_mark = true;
+}
+function do_submit(event){
+	if( my_mark){
+		my_mark = false;
+		return event.ctrlKey || confirm("This will delete the db entry. Continue?\n(Use Ctrl to override)");
+	}
+	return true;
+}
+function add_check(name){
+	var elements = document.getElementsByName(name);
+	for (var i = 0, elem = null; (elem = elements[i]); i++)
+		elem.onclick = do_mark;	
+}
