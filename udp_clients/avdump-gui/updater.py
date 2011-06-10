0f803ba70c3b055047fd75f9aaec864de03559aa
deridiot <deridiot@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2011-06-10 14:56:08 +0000

    git-svn-id: svn://dev.anidb.net/svn/trunk@4165 bed53a55-4da3-433c-baf4-d4f70f2fd34a

---
+++ udp_clients/avdump-gui/updater.py
@@ -0,0 +1,111 @@
+import os, urllib2, traceback, json, md5, httplib, sys, urllib
+
+class updater():
+    def __init__(self):
+        self._base_url    = 'http://static.anidb.net/client/avdump2/'
+        self._version_url = self._base_url + 'update.json'
+
+    def _get_response(self, url):
+        try:
+            request = urllib2.urlopen(url)
+            return request.read()    
+        except urllib2.HTTPError, e:
+            print 'Unable to get latest version info - HTTPError = ' + str(e.reason)
+            sys.exit(2)
+        except urllib2.URLError, e:
+            print 'Unable to get latest version info - URLError = ' + str(e.reason)
+            sys.exit(2)    
+        except httplib.HTTPException, e:
+            print 'Unable to get latest version info - HTTPException'
+            sys.exit(2)    
+        except Exception, e:
+            print 'Unable to get latest version info - Exception = ' + traceback.format_exc()
+            sys.exit(2)
+
+    def _download_file(self, filename):
+        try:
+            return urllib.urlretrieve(self._base_url + filename)
+        except urllib2.HTTPError, e:
+            print 'Unable to get latest version info - HTTPError = ' + str(e.reason)
+            sys.exit(2)
+        except urllib2.URLError, e:
+            print 'Unable to get latest version info - URLError = ' + str(e.reason)
+            sys.exit(2)    
+        except httplib.HTTPException, e:
+            print 'Unable to get latest version info - HTTPException'
+            sys.exit(2)    
+        except Exception, e:
+            print 'Unable to get latest version info - Exception = ' + traceback.format_exc()
+            sys.exit(2)
+
+    def _get_md5(self, filepath):
+        checksum = md5.new()
+        f = file(filepath, 'rb')
+
+        while True:
+            part = f.read(1024)
+            if not part:
+                break # end of file
+            checksum.update(part)
+        f.close()
+        return checksum.hexdigest()
+
+    def _md5_is_valid(self, org_md5, filename):
+        new_md5 = self._get_md5(filename)
+        print filename, org_md5, new_md5
+        if org_md5 == new_md5:
+            return True
+        else:
+            return False
+
+    def _update_files(self, files):
+        for avd2file in files:
+            if os.path.exists(agentFile['name']):
+                os.remove(agentFile['name'])
+
+            os.rename(agentFile['tempFile'], agentFile['name'])
+
+    def _get_updateInfo(self):
+        response = self._get_response(self._version_url)
+        if response is not None:
+            return json.loads(response)
+        else:
+            return None
+
+    def _get_localInfo(self):
+        locfiles = {}
+        for root, dir, files in os.walk('./dist'):
+            for filename in files:
+                checksum = self._get_md5(os.path.join(root, filename))
+                locfiles[filename] = checksum
+        return locfiles
+
+    def check_version(self):
+        localfiles = self._get_localInfo()
+        updateInfo = self._get_updateInfo()
+        if updateInfo is None:
+            print 'ERROR'
+            return
+
+        needUpdate = []
+        for avd2file in updateInfo['files']:
+            if avd2file['name'] in localfiles.keys():
+                if localfiles[avd2file['name']] != avd2file['md5']:
+                    needUpdate.append(avd2file)
+            else:
+                needUpdate.append(avd2file)
+
+        print needUpdate
+
+        files = []
+        for avd2file in needUpdate:
+            downloadedfile = self._download_file(avd2file['name'])[0]
+            if self._md5_is_valid(avd2file['md5'], downloadedfile) is True:
+                files.append(downloadedfile)
+
+        print files
+        #self._update_files(files)
+
+if __name__ == "__main__":
+    upd = updater()
+    upd.check_version()
