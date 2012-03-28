198b98f7d9818471c198611edb6e7aa898801820
fahrenheit <fahrenheit@bed53a55-4da3-433c-baf4-d4f70f2fd34a>
2012-03-28 21:56:58 +0000

    Added JSON support for ajax.js.
    For it to work correctly the correct method=json needs to be used.
    This lib also supports a basic fall back (without JSON revivers) for legacy browsers.
    
    A working example:
    
    xhttpRequestFetch(xhttpRequest(), someURL, function(reply) { alert(reply); }, null, "json")
    
    Note that the null is for the override, read the inline documentation for more help.
    
    git-svn-id: svn://dev.anidb.net/svn/trunk@4252 bed53a55-4da3-433c-baf4-d4f70f2fd34a

--- js-dev/ajax.js
+++ js-dev/ajax.js
@@ -5,14 +5,15 @@
  * version 1.0 (20.03.2007) - Initial Version
  * version 1.1 (30.04.2008) - Some minor cleanup
  * version 1.2 (24.04.2009) - Addition of paziek's XHR element, plus update of IE's MSXML methods.
+ * version 1.3 (28.03.2012) - Addition of support for JSON
  */
 jsVersionArray.push({
 	"file":"ajax.js",
-	"version":"1.2",
+	"version":"1.3",
 	"revision":"$Revision$",
 	"date":"$Date::                           $",
 	"author":"$Author$",
-	"changelog":"Addition of paziek's XHR element, plus update of IE's MSXML methods"
+	"changelog":"Addition of support for JSON"
 });
 
 // CORE Functions //
@@ -20,12 +21,33 @@ jsVersionArray.push({
 var isIE = false; // Are we dealing with an IE based browser?
 var isHeaderNeeded = true;
 var disableCaching = false; // Should we disable caching?
+var disableJSONrevivers = false; // Should we disable JSON revivers?
 
 /* Function that returns a new XMLHttpRequest object
  * @return an XMLHttpRequest object
  */
 function xhttpRequest() {
 	var httpRequest;
+	// test for JSON support and add it if it doesn't exist
+	if (!window.JSON) { // this is the basic version
+		window.JSON = {  
+			parse: function (sJSON) { return eval("(" + sJSON + ")"); },
+			stringify: function (vContent) {
+				if (vContent instanceof Object) {  
+					var sOutput = "";  
+					if (vContent.constructor === Array) {  
+						for (var nId = 0; nId < vContent.length; sOutput += this.stringify(vContent[nId]) + ",", nId++);  
+						return "[" + sOutput.substr(0, sOutput.length - 1) + "]";  
+					}  
+					if (vContent.toString !== Object.prototype.toString) { return "\"" + vContent.toString().replace(/"/g, "\\$&") + "\""; }  
+					for (var sProp in vContent) { sOutput += "\"" + sProp.replace(/"/g, "\\$&") + "\":" + this.stringify(vContent[sProp]) + ","; }  
+					return "{" + sOutput.substr(0, sOutput.length - 1) + "}";  
+				}  
+				return typeof vContent === "string" ? "\"" + vContent.replace(/"/g, "\\$&") + "\"" : String(vContent);  
+			}
+		}; 
+		disableJSONrevivers = true; // basic version
+	}
 	if (window.XMLHttpRequest) { // Mozilla, Safari, ...
 		httpRequest = new XMLHttpRequest();
 	} else if (window.ActiveXObject) { // IE
@@ -51,17 +73,18 @@ function xhttpRequest() {
  * @param url HTTP protcol URL
  * @param handler Function that will handle the results
  * @param override Overrides the document mime-type with given override
+ * @param method method (supported: xml, text, json)
  * @param data The Data to send to the URL
- * @param method method
+ * @param reviver An optional reviver function for parsing JSON data before sending it to the handler
  * @return void
  */
-function doRequest(obj, url, handler, override, method, data) {
+function doRequest(obj, url, handler, override, method, data, reviver) {
 	if (!obj) return;
 	try {
 		if(typeof obj.responseText === 'undefined') { // Checkes if we got XHR object, or its wrapper. Does this check works cross-browser? It should.
 			var xhr = obj.xhr;
 		} else var xhr = obj;
-		xhr.onreadystatechange = function() { xhttpRequestReadData(obj,handler,method); };
+		xhr.onreadystatechange = function() { xhttpRequestReadData(obj,handler,method, reviver); };
 		if (data) xhr.open('POST', url, true); // POST
 		else xhr.open('GET', url, true); // GET
 		if (xhr.overrideMimeType && override) xhr.overrideMimeType(override); // Setting the override type
@@ -79,22 +102,41 @@ function doRequest(obj, url, handler, override, method, data) {
 	}
 }
 
-/* Fetches a given url and then passes the result to the given handler */
-function xhttpRequestFetch(obj, url, handler, override, method) {
-	doRequest(obj, url, handler, override, method, null);
+/* Fetches a given url and then passes the result to the given handler
+ * @param obj The XMLHttpRequest object, or its wrapper (in that case, XHR should be in obj.xhr)
+ * @param url HTTP protcol URL
+ * @param handler Function that will handle the results
+ * @param override Overrides the document mime-type with given override
+ * @param method method (supported: xml, text, json)
+ * @param reviver An optional reviver function for parsing JSON data before sending it to the handler
+ * @return void
+ */
+function xhttpRequestFetch(obj, url, handler, override, method, reviver) {
+	doRequest(obj, url, handler, override, method, null, reviver);
 }
 
-/* Posts given data to a given url and then passes the result to the given handler */
-function xhttpRequestPost(obj, url, handler, data, override, method) {
-	doRequest(obj, url, handler, override, method, data)
+/* Posts given data to a given url and then passes the result to the given handler
+ * @param obj The XMLHttpRequest object, or its wrapper (in that case, XHR should be in obj.xhr)
+ * @param url HTTP protcol URL
+ * @param handler Function that will handle the results
+ * @param override Overrides the document mime-type with given override
+ * @param method method (supported: xml, text, json)
+ * @param data The Data to send to the URL
+ * @param reviver An optional reviver function for parsing JSON data before sending it to the handler
+ * @return void
+ */
+function xhttpRequestPost(obj, url, handler, data, override, method, reviver) {
+	doRequest(obj, url, handler, override, method, data, reviver)
 }
 
 /* This Function handles the status changes of the reply
  * @param obj The XMLHttpRequest object
  * @param handler The function that will handle the given obj
+ * @param method Data parse method, can be one of xml (for handling xml data), text or json (For handling json data)
+ * @param reviver An optional reviver function for parsing JSON data before sending it to the handler
  * @return void
  */
-function xhttpRequestReadData(obj, handler, method) {
+function xhttpRequestReadData(obj, handler, method, reviver) {
 	if (!obj) { xhttpRequestUpdateStatus('object is null...'); return; }
 	var rootDoc = 'root';
 //	try {
@@ -127,6 +169,11 @@ function xhttpRequestReadData(obj, handler, method) {
 							}
 						} else if (method == 'text') {
 							data = xhr.responseText;
+						} else if (method == 'json') {
+							if (reviver && !disableJSONrevivers)
+								data = JSON.parse(xhr.responseText, reviver);
+							else
+								data = JSON.parse(xhr.responseText);
 						}
 						if (handler) handler(data, obj);
 					break;
